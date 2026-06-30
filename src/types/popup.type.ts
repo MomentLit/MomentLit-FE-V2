38419de420cac2700293dbedc4bd472d88ae54c4
hyun731 <gilhyun.gwon0731@gmail.com>
2026-07-01 00:21:10 +0900

    cleanup: 이전 단일 API 및 타입 파일 제거

--- src/types/popup.type.ts
+++
@@ -1,9 +0,0 @@
-export type SpacePopupHistoryListSearchResponse = {
-  popup_id: number;
-  title: string;
-  thumbnail_url: string;
-  start_time: string;
-  end_time: string;
-  view_count: number;
-  like_count: number;
-};
