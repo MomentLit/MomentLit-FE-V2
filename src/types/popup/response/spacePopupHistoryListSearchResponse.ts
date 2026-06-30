f3effedac09416422ebb9cac602a62ed4b277b38
hyun731 <gilhyun.gwon0731@gmail.com>
2026-07-01 00:14:38 +0900

    refactor(types/popup): 팝업 응답 타입 분리

---
+++ src/types/popup/response/spacePopupHistoryListSearchResponse.ts
@@ -0,0 +1,9 @@
+export type SpacePopupHistoryListSearchResponse = {
+  popup_id: number;
+  title: string;
+  thumbnail_url: string;
+  start_time: string;
+  end_time: string;
+  view_count: number;
+  like_count: number;
+};
