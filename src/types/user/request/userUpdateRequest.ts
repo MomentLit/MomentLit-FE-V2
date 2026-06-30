3cf55cd705e7f77fabd9396d90af5b161ae0557a
hyun731 <gilhyun.gwon0731@gmail.com>
2026-07-01 00:12:55 +0900

    refactor(types/user): 사용자 요청 및 응답 타입 분리

---
+++ src/types/user/request/userUpdateRequest.ts
@@ -0,0 +1,6 @@
+export type UserUpdateRequest = {
+  name?: string | null;
+  image_url?: string | null;
+  phone?: string | null;
+  intro?: string | null;
+};
---
+++ src/types/user/response/signUpResponse.ts
@@ -0,0 +1,3 @@
+export type SignUpResponse = {
+  user_id: string;
+};
