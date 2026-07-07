d6c377e06b1505bf84acecee811f33346c7536f5
rlagywn <koduemg@gmail.com>
2026-07-07 14:55:14 +0900

    fix: default api rewrite to backend url

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
-const apiBaseUrl = process.env.API_BASE_URL?.replace(/\/$/, "");
+const apiBaseUrl =
+  process.env.API_BASE_URL?.replace(/\/$/, "") ??
+  "https://mo-d6a1ab80847946e8b30b1bf705b196c8.ecs.ap-northeast-2.on.aws";
 
 const nextConfig: NextConfig = {
   async rewrites() {
